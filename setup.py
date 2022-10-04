22ff9b1b5897d9423d3dc21e0cdf8913d86eec8f
mbagheri <mohammad.bagheri@oulu.fi>
2022-10-04 16:00:08 +0300

    update setup file

--- setup.py
+++ setup.py
@@ -1,27 +1,33 @@
-# setup.py
-import setuptools
+import os
+from distutils.core import Extension, setup
 
-
-setuptools.setup(
-name="corapy",
-version="0.1",
-author="Mohammad Bagheri",
-author_email="mohammad.bagheri@oulu.fi",
-description="Computational Raman Python tools",
-packages=["corapy","corapy/spectrum"],
-install_requires=["numpy","phonopy"],
-package_data={"": ["README.md","LICENSE","doc","example"]},
-url="http://ramandb.oulu.fi/",
-classifiers=[
-"Development Status :: ",
-"Intended Audience :: Science/Research",
-"License :: OSI Approved :: Creative Commons Attribution-ShareAlike 4.0 International License",
-"Operating System :: OS Independent",
-"Programming Language :: Python :: 3.5",
-"Programming Language :: Python :: 3.6",
-"Programming Language :: Python :: 3.7",
-"Programming Language :: Python :: 3.8",
-"Programming Language :: Python :: 3.9",
-"Topic :: Scientific/Engineering :: Materials",
-"Topic :: Scientific/Engineering :: Physics"]
+setup(
+    name="corapy",
+    version="0.1.0",
+    description="Computational Raman Python tools",
+    author="Mohammad Bagheri",
+    author_email="Mohammad.Bagheri@oulu.fi",
+    url="https://ramandb.oulu.fi",
+    python_requires=">=3.7",
+    install_requires=["numpy>=1.15.0","matplotlib>=2.2.2", "ase"],
+    packages=[
+        "corapy",
+        "corapy.spectrum",
+    ],
+    scripts=[
+        "scripts/corapy-plot",
+    ],
+    classifiers=[
+          "Development Status :: 1 - Beta",
+          "Intended Audience :: Science/Research",
+          "License :: OSI Approved :: BSD 3-Clause License",
+          "Operating System :: OS Independent",
+          "Programming Language :: Python :: 3.5",
+          "Programming Language :: Python :: 3.6",
+          "Programming Language :: Python :: 3.7",
+          "Programming Language :: Python :: 3.8",
+          "Programming Language :: Python :: 3.9",
+          "Topic :: Scientific/Engineering :: Materials",
+          "Topic :: Scientific/Engineering :: Physics",
+          ],
 )
